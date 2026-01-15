d145af7e41ba5c9f20b02134f9e1025c076defff
Dhanashree Jadhav <dhanaa1611@gmail.com>
2026-01-15 21:13:10 +0530

    Add basic HTTP server in app.py

---
+++ org-docker-app/app.py
@@ -0,0 +1,13 @@
+student@DESKTOP-3GMCIP9:~/org-docker-app$ cat app.py
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write("Hello from Dockerized App 🚀".encode("utf-8"))
+
+server = HTTPServer(("0.0.0.0", 8000), MyHandler)
+print("Server running on port 8000")
+server.serve_forever()
